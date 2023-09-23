8e9ea4c60eb83af6a476cc6552640fe58199b2e8
BrianHebert <butterflyrbrian40@gmail.com>
2023-09-23 13:25:14 -0500

    added the ablility to log out, also login button doesnt show up when logged in and log out doesnt show up when logged out

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { auth, provider } from "../config/firebase"
 import { useAuthState } from 'react-firebase-hooks/auth'
 import { signInWithPopup } from "firebase/auth";
+import { signOut } from "firebase/auth";
 
 export default function Navbar(){
 
@@ -11,13 +12,29 @@ export default function Navbar(){
     }
 
     const [user] = useAuthState(auth)
+    async function signUserOut (){
+        await signOut(auth)
+
+    }
 
 
     return(
         <div>
+            {!user && 
+            <div>
             <button onClick={signInWithGoogle}>Login</button>
+            </div>}
+
+            {user && 
+            <div>
             <p>{user?.displayName}</p>
             <img src={user?.photoURL || ""} width={30} height={30}/>
+            </div>}
+
+            {user && 
+            <div>
+            <button onClick={signUserOut}>Log Out</button>
+            </div>}
         </div>
     )
 }
